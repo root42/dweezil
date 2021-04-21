0e0d698de39207c71ce9ad2c653f047dce9f5e74
Arne Schmitz <arne.schmitz@gmx.net>
2021-04-21 20:49:56 +0200

    support keys R and Z for toggling zoom and rotation

--- DWEEZIL.C
+++ DWEEZIL.C
@@ -14,15 +14,21 @@
 #define SCREEN_HEIGHT 200
 
 #define PIECE_SIZE 16
-#define NUM_PIECES 15
-#define BUF_WIDTH 15*16
+#define NUM_PIECES_X 15
+#define NUM_PIECES_Y 15
+
+const word buf_width = PIECE_SIZE * NUM_PIECES_X;
+const word buf_height = PIECE_SIZE * NUM_PIECES_Y;
 
 byte far *framebuf;
 byte far *data_chunks;
 byte pal[768];
 
-#define SETPIX(x,y,c) *(framebuf + (dword)BUF_WIDTH * (y) + (x)) = c
-#define GETPIX(x,y) *(framebuf + (dword)BUF_WIDTH * (y) + (x))
+int rotation = 1;
+int zoom = -1;
+
+#define SETPIX(x,y,c) *(framebuf + (dword)buf_width * (y) + (x)) = c
+#define GETPIX(x,y) *(framebuf + (dword)buf_width * (y) + (x))
 
 void
 draw_dweezil()
@@ -31,11 +37,9 @@ draw_dweezil()
   byte color;
   static int pos = 0;
   int xs, ys, xd, yd, s;
-  int w = PIECE_SIZE * NUM_PIECES;
-  int h = PIECE_SIZE * NUM_PIECES;
+  int w = buf_width;
+  int h = buf_height;
   byte shift;
-  int rotation = 1;
-  int zoom = -1;
 
   /* shuffle image center deterministically */
   pos++;
@@ -57,15 +61,15 @@ draw_dweezil()
     }
   }
 
-  for( y = 0; y < NUM_PIECES; y++ ) {
-    for( x = 0; x < NUM_PIECES; x++ ) {
-      xs = x * PIECE_SIZE + shift + (x - (NUM_PIECES>>1)) * zoom - (y - (NUM_PIECES>>1)) * rotation;
-      ys = y * PIECE_SIZE + shift + (y - (NUM_PIECES>>1)) * zoom + (x - (NUM_PIECES>>1)) * rotation;
+  for( y = 0; y < NUM_PIECES_Y; y++ ) {
+    for( x = 0; x < NUM_PIECES_X; x++ ) {
+      xs = x * PIECE_SIZE + shift + (x - (NUM_PIECES_X / 2)) * zoom - (y - (NUM_PIECES_Y / 2)) * rotation;
+      ys = y * PIECE_SIZE + shift + (y - (NUM_PIECES_Y / 2)) * zoom + (x - (NUM_PIECES_X / 2)) * rotation;
       xd = x * PIECE_SIZE;
       yd = y * PIECE_SIZE;
       memcpy_rect(
 	data_chunks, framebuf,
-	BUF_WIDTH, BUF_WIDTH,
+	buf_width, buf_height,
 	xs, ys,
 	xd, yd,
 	PIECE_SIZE, PIECE_SIZE
@@ -73,15 +77,15 @@ draw_dweezil()
     }
   }
 
-  for( y = 0; y < NUM_PIECES; y++ ) {
-    for( x = 0; x < NUM_PIECES; x++ ) {
+  for( y = 0; y < NUM_PIECES_Y; y++ ) {
+    for( x = 0; x < NUM_PIECES_X; x++ ) {
       xs = x * PIECE_SIZE;
       ys = y * PIECE_SIZE;
       xd = x * PIECE_SIZE + shift;
       yd = y * PIECE_SIZE + shift;
       memcpy_rect(
 	framebuf, data_chunks,
-	BUF_WIDTH, BUF_WIDTH,
+	buf_width, buf_height,
 	xs, ys,
 	xd, yd,
 	PIECE_SIZE, PIECE_SIZE
@@ -93,10 +97,9 @@ draw_dweezil()
 int
 main()
 {
-  byte do_quit = 0;
+  char kc = 0;
   int i, y;
-  const int sz = PIECE_SIZE * NUM_PIECES;
-  word buf_size = sz * sz;
+  const word buf_size = buf_width * buf_height;
 
   framebuf = farmalloc( buf_size );
   data_chunks = farmalloc( buf_size );
@@ -109,18 +112,29 @@ main()
   set_palette( fire_pal );
   memset(framebuf, 0x00, buf_size);
   memset(data_chunks, 0x00, buf_size);
-  while( !do_quit ) {
-    if(kbhit()){
-      getch();
-      do_quit = 1;
+  while( kc != 0x1b ) {
+    if( kbhit() ) {
+      kc = getch();
+      switch( kc ) {
+      case 'r':
+	rotation++;
+	if( rotation > 1 ) rotation = -1;
+	break;
+      case 'z':
+	zoom++;
+	if( zoom > 1 ) zoom = -1;
+	break;
+      default:
+	break;
+      }
     }
     draw_dweezil();
     wait_for_retrace();
-    for( y = 0; y < (sz < 200 ? sz : 200); ++y ) {
+    for( y = 0; y < (buf_height < 200 ? buf_height : 200); ++y ) {
       memcpy(
 	VGA + y * SCREEN_WIDTH,
-	framebuf + y * sz,
-	sz
+	framebuf + y * buf_width,
+	buf_width
       );
     }
   }
